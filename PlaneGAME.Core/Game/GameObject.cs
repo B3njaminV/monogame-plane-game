304da24d518edd80473533dfece5e13a651caaa3
Jules MIGNOTTE <Jules.MIGNOTTE@etu.uca.fr>
2022-03-01 17:09:01 +0100

    kdo

---
+++ PlaneGAME.Core/Game/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlaneGAME.Core.Game
+{
+    public class GameObject : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        public SpriteBatch SpriteBatch { get => spriteBatch; set => spriteBatch = value; }
+
+
+        public GameObject(Microsoft.Xna.Framework.Game game, SpriteBatch spritebatch) : base(game)
+        {
+            spriteBatch = spritebatch;
+        }
+    }
+}
